678924c766a6c3e46c8ad0bb15de8808ba802e31
Justin Pumford <jpumford@gmail.com>
2024-01-07 16:52:36 -0800

    add template

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,22 +1,77 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig, PluginRef } from "gatsby"
+import "dotenv/config"
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `1806 W Dalmeny Way`,
-    siteUrl: `https://www.yourdomain.tld`
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.mjs
+    siteTitle: `Jodie`,
+    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
+    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
+    siteUrl: `https://jodie.lekoarts.de`,
+    siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+    siteImage: `/banner.jpg`,
+    siteLanguage: `en`,
+    author: `@lekoarts_de`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  trailingSlash: `always`,
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-jodie`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          { name: `Projects`, slug: `/projects` },
+          { name: `Art`, slug: `/art` },
+          { name: `About`, slug: `/about` },
+        ],
+      },
     },
-    __key: "pages"
-  }]
-};
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `jodie - @lekoarts/gatsby-theme-jodie`,
+        short_name: `jodie`,
+        description: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#b75e09`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    // You can remove this plugin if you don't need it
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-statoscope`,
+      options: {
+        saveReportTo: `${__dirname}/public/.statoscope/_bundle.html`,
+        saveStatsTo: `${__dirname}/public/.statoscope/_stats.json`,
+        open: false,
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>,
+}
 
-export default config;
+export default config
